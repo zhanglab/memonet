d3c3b2f3b4785b7c490b590e477816357a38223d
kdunton_uri_edu <kdunton_uri_edu@login1.unity.rc.umass.edu>
2022-09-26 19:47:30 +0000

    added scale step

--- scripts/DESC_highRankGenes_step1_loop15.py
+++ scripts/DESC_highRankGenes_step1_loop15.py
@@ -1,4 +1,5 @@
 # this script is for running initial DESC clusters back through DESC to get subclusters
+# also calculates silhouette score for cluster results
 
 
 import desc as desc
@@ -8,40 +9,19 @@ import scanpy as sc
 import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
+import episcanpy
 sc.settings.verbosity = 3  #verbosity: errors(0), warnings(1), info(2), hints(3)
 sc.logging.print_versions()
 
 
-
-######### load andata #########
-#path = '/data/zhanglab/kdunton/6samples_cluster/deepseq_3_clustering/DESC/cluster_by_genes/ourL23train/classifier-train_vs_control_3.8.22/cluster_TrCtrl_together_noDelta3.24.22/test_DESC_rerun/TrCtrl_noDelta_classifierGenesOnly.csv'
-#path = '/data/zhanglab/kdunton/6samples_cluster/deepseq_3_clustering/DESC/cluster_by_genes/ourL23train/classifier-train_vs_control_3.8.22/cluster_TrCtrl_together_noDelta3.24.22/test_DESC_decimals/TrCtrl_noDelta_classifierGenesOnly_7dec.csv'
-#path = '/data/zhanglab/kdunton/6samples_cluster/deepseq_3_clustering/DESC/cluster_by_genes/ourL23train/classifier-train_vs_control_3.8.22/cluster_TrCtrl_together_noDelta3.24.22/TrCtrl_noDelta_classifierGenesOnly.csv'
-
-#print("Path:")
-#print(path)
-#adata = sc.read(path)
-  #this reads a counts matrix into anndata object; has to have cells in rownames and genes in columns
-
-
-######### preprocessing #########
-#the counts in adata have already been normalized by highRankGenes.r
-
-## view object
-#print("adata initial")
-#print(adata)
-
-#adata.raw = adata
-
-
-n_neighbors = [8,10,15,18,19,20,21,22,23,24,25]
+######## set parameters to loop through ########
+n_neighbors = [8,9,10,11,12,13,14,15,18,19,20,21,22,23,24,25]
 #n_neighbors = [21,22,23,24,25]
 n_neighbors = np.array(n_neighbors)
-#louvain_resolution = [0.7,0.71,0.72,0.73,0.74,0.75,0.76,0.77,0.78,0.79,
- #                     0.8,0.81,0.82,0.83,0.84,0.85,0.86,0.87,0.88,0.89,
-  #                    0.9]
 louvain_resolution = [0.7,0.75,0.8,0.85,0.9]
 louvain_resolution = np.array(louvain_resolution)
+
+
 
 ######### run DESC #########
 for n in n_neighbors:
@@ -49,15 +29,19 @@ for n in n_neighbors:
   for res in louvain_resolution:
     print("louvain_resolution=" + str(res))
 
-    #path = '/work/pi_yingzhang_uri_edu/kdunton/RNAseq/data/memonet_data/counts_matrices/L23_DGmtx_dec15.csv'
-    path = '/work/pi_yingzhang_uri_edu/kdunton/RNAseq/cluster_by_genes/DESC_parameter_test/L23_0.3_DGmtx_dec15.csv'
+    path = '/work/pi_yingzhang_uri_edu/kdunton/RNAseq/cluster_by_genes/0.3cutoff/L23_0.3_DGmtx.csv'
     print("Path:"+ str(path))
     adata = sc.read(path)
     print("adata initial")
     print(adata)
     adata.raw = adata
 
-    save_dir = ["result_DESC15.n"+str(n)+".L"+str(res)]
+    ## z-score the genes
+    desc.scale(adata, zero_center=True, max_value=3)
+
+    
+
+    save_dir = ["result_DESC22.n"+str(n)+".L"+str(res)]
       #save_dir is a list
     #convert to string
     save_dir = ''.join(map(str,save_dir))
@@ -73,45 +57,49 @@ for n in n_neighbors:
     #Umap: plot the clusterIDs
     basis = ["umap" + str(res)]
     basis = ''.join(map(str,basis))
-    print(basis)
     color = ["desc_" + str(res)]
     color = ''.join(map(str,color))
-    print(color)
-    save = ["desc.dec15.n" + str(n) + ".L" + str(res) + ".png"]
+    save = ["desc.dec22.n" + str(n) + ".L" + str(res) + ".png"]
     print(save)
     save = ''.join(map(str,save))
 
     sc.pl.scatter(adata, basis=basis, color=color, save = save)
 
 
-######### save result #########
-## save AnnData object into .h5ad file ##
-#adata.write('desc_result.h5ad')
 
-## save into csv ##
-# tsne coordinates:
-    #tsne = ["X_tsne" + str(res)]
-    #tsne = ''.join(map(str,tsne))
-    #obsm_data=pd.DataFrame(adata.obsm[tsne])
-    #save_tsne = ["tsne_dec15.n" + str(n) + ".L" + str(res) + ".csv"]
-    #save_tsne = ''.join(map(str,save_tsne))
-    #obsm_data.to_csv(save_tsne,sep=",")
-
-# umap coordinates:
+    ######### save result #########
+    # umap coordinates:
     umap = ["X_umap" + str(res)]
     umap = ''.join(map(str,umap))
     obsm_data=pd.DataFrame(adata.obsm[umap])
-    save_umap = ["umap_dec15.n" + str(n) + ".L" + str(res) + ".csv"]
+    save_umap = ["umap_dec22.n" + str(n) + ".L" + str(res) + ".csv"]
     save_umap = ''.join(map(str,save_umap))
     obsm_data.to_csv(save_umap,sep=",")
 
-# clustering result/labels:
+    # clustering result/labels:
     clu = ["desc_" + str(res)]
     clu = ''.join(map(str,clu))
     obs_data=pd.DataFrame(adata.obs[clu])
-    save_clu = ["clusters_dec15.n" + str(n) + ".L" + str(res) + ".csv"]
+    save_clu = ["clusters_dec22.n" + str(n) + ".L" + str(res) + ".csv"]
     save_clu = ''.join(map(str,save_clu))
     obs_data.to_csv(save_clu,sep=",")
+
+
+
+    ######### calculate silhouette score #########
+    cluster_annot = ['desc_' + str(res)]
+    cluster_annot = ''.join(map(str,cluster_annot))
+    value = ['X_Embeded_z' + str(res)]
+    value = ''.join(map(str,value))
+    episcanpy.tl.silhouette(adata, cluster_annot=cluster_annot, value=value)
+      # saves individual silhouette scores to obs, with cluster IDs
+
+    sil = pd.DataFrame(adata.obs)
+    save_sil = ["sil_scores_n" + str(n) + ".L" + str(res) + ".csv"]
+    save_sil = ''.join(map(str,save_sil))
+    sil.to_csv(save_sil,sep=',')
+    print("************ saved sil ***************")
+    print(save_sil)
 
 
 print("done!")
