55eb9ec4802b745ff50da8aab3f99afd99401fda
kdunton_uri_edu <kdunton_uri_edu@login1.unity.rc.umass.edu>
2022-09-27 20:57:51 +0000

    added import episcanpy

--- scripts/DESC_DGclustering.py
+++ scripts/DESC_DGclustering.py
@@ -8,6 +8,7 @@ import scanpy as sc
 import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
+import episcanpy
 sc.settings.verbosity = 3  #verbosity: errors(0), warnings(1), info(2), hints(3)
 sc.logging.print_versions()
 
