77a91d310634c85e92652c55dd0e00d92e1813f3
kdunton_uri_edu <kdunton_uri_edu@login2.unity.rc.umass.edu>
2022-09-29 21:08:40 +0000

    changed names of DESeq2 scripts to be more concise

--- scripts/DESeq2_clusterX_vs_clusterY1and2.sh
+++ scripts/DESeq2_clusterX_vs_clusterY1and2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -p uri-cpu,cpu,cpu-preempt
-#SBATCH -c 2
 #SBATCH --export=NONE
+##SBATCH -q zhanglab
 #SBATCH --mail-type=END
 #SBATCH --mail-user=kdunton@uri.edu
 
@@ -9,7 +9,7 @@
 #it won't run correctly with multiple nodes if you're using BiocParallel
 
 #usage: ANDROMEDA or bluewaves
-#       sbatch --exclusive --mem=120G /work/pi_yingzhang_uri_edu/kdunton/memonet_github_repo/memonet/scripts/DESeq2_clusterX_vs_clusterY_whole.sh
+#       sbatch --exclusive --mem=120G /work/pi_yingzhang_uri_edu/kdunton/memonet_github_repo/memonet/scripts/DESeq2_clusterX_vs_clusterY1and2.sh
 #         takes ~20min  on andromeda and unity
 
 #module load R-bundle-Bioconductor/3.10-foss-2019b
@@ -19,4 +19,4 @@ conda activate DESeq2
 
 export OMP_NUM_THREADS=1
 
-Rscript /work/pi_yingzhang_uri_edu/kdunton/memonet_github_repo/memonet/scripts/DESeq2_clusterX_vs_clusterY_whole.r
+Rscript /work/pi_yingzhang_uri_edu/kdunton/memonet_github_repo/memonet/scripts/DESeq2_clusterX_vs_clusterY1and2.r
--- scripts/DESeq2_clusterX_vs_others.sh
+++ scripts/DESeq2_clusterX_vs_others.sh
@@ -8,7 +8,7 @@
 #it won't run correctly with multiple nodes if you're using BiocParallel
 
 #usage: ANDROMEDA or bluewaves (might work at 120G)
-#       sbatch --exclusive --mem=250G /work/pi_yingzhang_uri_edu/kdunton/memonet_github_repo/memonet/scripts/DESeq2_whole_subclusters.sh
+#       sbatch --exclusive --mem=250G /work/pi_yingzhang_uri_edu/kdunton/memonet_github_repo/memonet/scripts/DESeq2_clusterX_vs_others.sh
 #         takes ~1hr 40min on andromeda
 
 module load miniconda
@@ -17,4 +17,4 @@ conda activate DESeq2
 
 export OMP_NUM_THREADS=1
 
-Rscript /work/pi_yingzhang_uri_edu/kdunton/memonet_github_repo/memonet/scripts/DESeq2_whole_subclusters.r
+Rscript /work/pi_yingzhang_uri_edu/kdunton/memonet_github_repo/memonet/scripts/DESeq2_clusterX_vs_others.r
