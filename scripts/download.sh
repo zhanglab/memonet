0bc29f3f55ea6c65513f396c2f8286d6c32828f3
kdunton <65360195+kdunton@users.noreply.github.com>
2022-10-05 11:29:46 -0400

    download memonet 10X data

---
+++ scripts/download.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#wget --no-check-certificate -nH -np -r --cut-dirs=2 http://epigenomics.sdsc.edu/zhc268/lims_singlecell/t9FEunKf_JB_262_1_2/
+#wget --no-check-certificate -nH -np -r --cut-dirs=2 http://epigenomics.sdsc.edu/zhc268/lims_singlecell/XvAkv9K3_JB_263_1_2/
+#wget --no-check-certificate -nH -np -r --cut-dirs=2 http://epigenomics.sdsc.edu/zhc268/lims_singlecell/slPsiwmg_JB_262_1_2_3/
+#wget --no-check-certificate -nH -np -r --cut-dirs=2 http://epigenomics.sdsc.edu/zhc268/lims_singlecell/QF9GKKgY_JB_263_1_2_3/
+#wget --no-check-certificate -nH -np -r --cut-dirs=2 http://epigenomics.sdsc.edu/zhc268/lims_singlecell/7NanXV4A_JB_276_1_2/
+#wget --no-check-certificate -nH -np -r --cut-dirs=2 http://epigenomics.sdsc.edu/zhc268/lims_singlecell/oyn2DkZf_JB_277_1_2/
+#wget --no-check-certificate -nH -np -r --cut-dirs=2 http://epigenomics.sdsc.edu/zhc268/lims_singlecell/LVwmju0H_JB_278_1_2/
+#wget --no-check-certificate -nH -np -r --cut-dirs=2 http://epigenomics.sdsc.edu/zhc268/lims_singlecell/FHuOVucK_JB_279_1_2/
+wget --no-check-certificate -nH -np -r --cut-dirs=2 http://epigenomics.sdsc.edu/jbuchanan/Komiyama/cellranger/JB_276_1_2_3/outs/
+wget --no-check-certificate -nH -np -r --cut-dirs=2 http://epigenomics.sdsc.edu/jbuchanan/Komiyama/cellranger/JB_277_1_2_3/outs/
+wget --no-check-certificate -nH -np -r --cut-dirs=2 http://epigenomics.sdsc.edu/jbuchanan/Komiyama/cellranger/JB_278_1_2_3/outs/
