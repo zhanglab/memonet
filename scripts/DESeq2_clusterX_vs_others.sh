5d95056f7798081d9a123f43aa8cbfff74b06a78
kdunton <65360195+kdunton@users.noreply.github.com>
2022-11-22 11:17:53 -0500

    Update DESeq2_clusterX_vs_others.sh

--- scripts/DESeq2_clusterX_vs_others.sh
+++ scripts/DESeq2_clusterX_vs_others.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
-#SBATCH -p uri-cpu,cpu,cpu-preempt
 #SBATCH --export=NONE
-#SBATCH --mail-type=END
-#SBATCH --mail-user=kdunton@uri.edu
 
 
 #it won't run correctly with multiple nodes if you're using BiocParallel
 
-#usage: ANDROMEDA or bluewaves (might work at 120G)
-#       sbatch --exclusive --mem=250G /work/pi_yingzhang_uri_edu/kdunton/memonet_github_repo/memonet/scripts/DESeq2_clusterX_vs_others.sh
-#         takes ~1hr 40min on andromeda
+#usage:
+#       sbatch --exclusive --mem=250G <PATH>/DESeq2_clusterX_vs_others.sh
+#         takes ~1hr 40min 
 
-module load miniconda
-conda activate DESeq2
+# load miniconda
+# load DESeq2 and other dependencies listed in the R script
 
 
 export OMP_NUM_THREADS=1
 
-Rscript /work/pi_yingzhang_uri_edu/kdunton/memonet_github_repo/memonet/scripts/DESeq2_clusterX_vs_others.r
+Rscript <PATH>/DESeq2_clusterX_vs_others.r
