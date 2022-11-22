279637f26a19274dc63c5508a18a9b4e7d71e628
kdunton <65360195+kdunton@users.noreply.github.com>
2022-11-22 11:15:12 -0500

    Update DESeq2-table.sh

--- scripts/DESeq2-table.sh
+++ scripts/DESeq2-table.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #SBATCH --export=NONE
-#SBATCH -p uri-cpu,cpu,cpu-preempt
 
 
-#usage: sbatch --mem=120G /work/pi_yingzhang_uri_edu/kdunton/memonet_github_repo/memonet/scripts/DESeq2-table.sh
+#usage: sbatch --mem=120G <PATH>/DESeq2-table.sh
 
 
-module load miniconda
-conda activate DESeq2
+#load miniconda
+#load DESeq2 and other dependences listed in the R script
 
 
-Rscript /work/pi_yingzhang_uri_edu/kdunton/memonet_github_repo/memonet/scripts/DESeq2-table.r
+Rscript <PATH>/DESeq2-table.r
