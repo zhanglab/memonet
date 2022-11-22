700608ead823f430750657c098d8cee6e9d4bb4b
kdunton <65360195+kdunton@users.noreply.github.com>
2022-11-22 14:04:26 -0500

    Add files via upload

---
+++ scripts/mapping_OA.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#SBATCH --mem=250G 
+##SBATCH -c 4
+#SBATCH --export=NONE
+##SBATCH -q zhanglab
+#SBATCH --exclusive
+#SBATCH --mail-type=END
+#SBATCH --mail-user=kdunton@uri.edu
+
+
+# using -q zhanglab means it can't use n107
+
+# usage: andromeda
+#        sbatch /data/zhanglab/kdunton/neuron_model/katie-scripts/RNAseq/pipeline_scripts/Andromeda/mapping_OA.sh
+# takes ~ 1hr at 120G, 30min at 250G
+
+#module load scCATCH/2.1-foss-2019b-R-3.6.2
+module load scCATCH/2.1-foss-2020b-R-4.0.3
+  #this is Seurat v4
+
+Rscript /data/zhanglab/kdunton/neuron_model/katie-scripts/RNAseq/pipeline_scripts/Andromeda/mapping_OA.r
