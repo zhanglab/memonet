957f86ef0b0a3206d881656d6d382a3eb3f966c5
kdunton <65360195+kdunton@users.noreply.github.com>
2022-11-22 11:21:37 -0500

    Update QCthresholds.sh

--- scripts/QCthresholds.sh
+++ scripts/QCthresholds.sh
@@ -2,10 +2,10 @@
 #SBATCH --mem=250G
 #SBATCH --export=NONE
 
-# usage: sbatch ~/Downloads/RNAseq/memonet_github_repo/memonet/scripts/QCthresholds.sh
-
-module load scCATCH/2.1-foss-2019b-R-3.6.2
-  #andromeda
+# usage: sbatch <PATH>/QCthresholds.sh
 
 
-Rscript ~/Downloads/RNAseq/memonet_github_repo/memonet/scripts/QCthresholds.r
+# load Seurat
+
+
+Rscript <PATH>/QCthresholds.r
